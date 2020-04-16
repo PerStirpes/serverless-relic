9bf458e1cad9b5429dde3197187c825134b2f463
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2020-04-16 11:22:58 -0700

    switch to dynamo

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -1,5 +1,13 @@
 const { ApolloServer, gql } = require("apollo-server-lambda")
+const AWS = require("aws-sdk")
+const { v4: uuidv4 } = require("uuid")
 
+AWS.config.update({
+    region: "us-east-2",
+    accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY
+  });
+  
 const typeDefs = gql`
     type Query {
         todos: [Todo]!
@@ -19,24 +27,82 @@ const todos = {}
 let todoIndex = 0
 const resolvers = {
     Query: {
-        todos: (parent, args, { user }) => {
+        todos: async (parent, args, { user }) => {
             if (!user) {
                 return []
             } else {
-                return Object.values(todos)
+                const params = {
+                    TableName: table,
+                    KeyConditionExpression: "pk = :userid and begins_with(sk, :todokey)",
+                    ExpressionAttributeValues: {
+                        ":userid": `user#${user}`,
+                        ":todokey": "todo#",
+                    },
+                }
+                const result = await docClient.query(params).promise()
+                return result.Items.map(({ pk, sk, data }) => {
+                    return {
+                        id: sk.replace("todo#", ""),
+                        ...data,
+                    }
+                })
             }
         },
     },
     Mutation: {
-        addTodo: (_, { text }) => {
-            todoIndex++
-            const id = `key-${todoIndex}`
-            todos[id] = { id, text, done: false }
-            return todos[id]
+        addTodo: async (_, { text }, { user }) => {
+            if (!user) {
+                throw new Error("Must be authenticated to insert todos")
+            }
+            const todoUuid = uuidv4()
+            const params = {
+                TableName: table,
+                Item: {
+                    pk: `user#${user}`,
+                    sk: `todo#${todoUuid}`,
+                    data: {
+                        createdAt: Date.now(),
+                        updatedAt: Date.now(),
+                        done: false,
+                        text,
+                    },
+                },
+            }
+            await docClient.put(params).promise()
+            return {
+                id: todoUuid,
+                done: false,
+                text,
+            }
         },
-        updateTodoDone: (_, { id }) => {
-            todos[id].done = true
-            return todos[id]
+        updateTodoDone: async (_, { id }, { user }) => {
+            if (!user) {
+                throw new Error("Must be authenticated to insert todos")
+            }
+            const params = {
+                TableName: table,
+                Key: {
+                    pk: `user#${user}`,
+                    sk: `todo#${id}`,
+                },
+                UpdateExpression: "set #data.#done = :newdone",
+                ExpressionAttributeNames: {
+                    "#data": "data",
+                    "#done": "done",
+                },
+                ExpressionAttributeValues: {
+                    ":newdone": true,
+                },
+                ReturnValues: "ALL_NEW",
+            }
+            const result = await docClient.update(params).promise()
+
+            const { pk, sk, data } = result.Attributes
+            return {
+                id: sk.replace("todo#", ""),
+                text: data.text,
+                done: data.done,
+            }
         },
     },
 }
