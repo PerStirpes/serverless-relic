9db9244d5c5d0b0eb6d109c65e03bc45335afe49
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2020-04-16 09:02:37 -0700

    Add mutation

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -2,19 +2,45 @@ const { ApolloServer, gql } = require("apollo-server-lambda")
 
 const typeDefs = gql`
     type Query {
-        hello: String
+        todos: [Todo]!
+    }
+    type Todo {
+        id: ID!
+        text: String!
+        done: Boolean!
+    }
+    type Mutation {
+        addTodo(text: String!): Todo
+        updateTodoDone(id: ID!): Todo
     }
 `
+
+const todos = {}
+let todoIndex = 0
+
 const resolvers = {
     Query: {
-        hello: () => "Hello world!",
+        todos: () => {
+            return Object.values(todos)
+        },
+    },
+    Mutation: {
+        addTodo: (_, { text }) => {
+            todoIndex++
+            const id = `key-${todoIndex}`
+            todos[id] = { id, text, done: false }
+            return todos[id]
+        },
+        updateTodoDone: (_, { id }) => {
+            todos[id].done = true
+            return todos[id]
+        },
     },
 }
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-
     playground: true,
     introspection: true,
 })
